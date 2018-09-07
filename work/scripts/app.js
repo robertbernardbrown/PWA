a993ef00f3be826b44eb62a5593a6bc97f2f5eb3
Bob Brown <robertbernardbrown@gmail.com>
2018-09-07 14:54:36 -0400

    adds servie worker logic and files to cache for service worker

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -334,4 +334,9 @@
   }
 
   // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
 })();
